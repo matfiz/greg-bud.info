80d557c5c1c7e9fbb3b96b3a25b853d450165302
Mariusz Henn <mariusz@IBM>
2013-07-16 11:22:47 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GregBudInfo::Application.config.secret_token = '46e8635d3db9ec477dd28ab24ad7a204ef5b5ce46baed65367895d97a65301db3d784a3aed78817fd54a60f9efe1cbbe3baa223f83570f88a70c8edf66154ab2'
