540f94e3ca6571869c248d324fec540664261b1f
Mariusz Henn <mariusz@IBM>
2013-07-30 16:18:23 +0200

    capify

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :rails_env,     ENV['rails_env'] || ENV['RAILS_ENV'] || default_env
 set :gemhome, "/home/grawerpix/ruby/gems"
 set :gempath, "/home/grawerpix/ruby/gems"
 set :rake, "source /home/grawerpix/.bash_profile && GEM_HOME=/home/grawerpix/ruby/gems rake"
-set :rvm_ruby_string, 'ruby-1.9.3@greg-bud.info'
+set :rvm_ruby_string, 'ruby-1.9.3'
+set :rvm_gemset_name, 'greg-bud.info'
 #set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 set :rvm_type, :system
 #set :rvm_path, '/usr/local/rvm'
