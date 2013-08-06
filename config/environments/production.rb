80cfa99a5ff323dfc646ff70998a44fdc20d30ac
Mariusz Henn <mariusz@IBM>
2013-08-06 15:05:49 +0200

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-GregBudInfo::Application.configure do
+GrawerpixPl::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
@@ -10,13 +10,11 @@ GregBudInfo::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
-
   # Compress JavaScripts and CSS
   config.assets.compress = true
-
+  
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
   # Generate digests for assets URLs
   config.assets.digest = true
 
@@ -46,18 +44,24 @@ GregBudInfo::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-
+  config.assets.precompile += %w( _subpage.js )
+  config.assets.precompile += ['galleria/*'] 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
-
+  
+  # Disable delivery errors, bad email addresses will be ignored
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'greg-bud.info' }
+  c#onfig.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.perform_deliveries = true
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-
+  config.i18n.available_locales = [:pl, :de, :en]
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
