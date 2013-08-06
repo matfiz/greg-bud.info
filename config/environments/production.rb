43024e44bc83f3581232b635242af9b8e9dd11bd
Mariusz Henn <mariusz@IBM>
2013-08-06 15:07:17 +0200

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-GrawerpixPl::Application.configure do
+GregBudInfo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
